16359e311c205820e0d62af1c9b79c326acbb7eb
mudpuppy12 <dennis@demarco.com>
2024-09-14 18:11:37 -0500

    Cleaned up providers to auto-generate

--- components/terraform/vpc/variables.tf
+++ components/terraform/vpc/variables.tf
@@ -8,13 +8,6 @@ variable "stage" {
   type        = string
 }
 
-# AWS Region
-variable "region" {
-  description = "Region in which AWS Resources to be created"
-  type        = string
-  default     = "us-east-1"
-}
-# Environment Variable
 variable "environment" {
   description = "Environment Variable used as a prefix"
   type        = string
