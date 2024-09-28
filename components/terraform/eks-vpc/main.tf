c843b39656e4c8dc597775a72b98ea76b92d7af6
mudpuppy12 <dennis@demarco.com>
2024-09-28 16:04:14 +0000

    Removed uneeded tagging

--- components/terraform/eks-vpc/main.tf
+++ components/terraform/eks-vpc/main.tf
@@ -31,7 +31,6 @@ module "vpc" {
   private_subnet_tags = {
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
     "kubernetes.io/role/internal-elb"           = 1
-    SSM                                         = "Y"
   }
 
   tags = local.tags
